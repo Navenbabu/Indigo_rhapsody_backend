2b966b3f7387e70fe600cc476a64fa605017c518
Flutterneu <rsmx141@gmail.com>
2024-11-07 18:12:02 +0530

    up[dated

--- src/routes/stateRoutes.js
+++ src/routes/stateRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const stateController = require("../controllers/stateController");
 
-router.post("/", stateController.createState);
+router.post("/", stateController.CreateState);
 router.get("/", stateController.getStates);
 
 module.exports = router;
