5e6c588ec07abdb54a40cb34951956265637da6f
Flutterneu <rsmx141@gmail.com>
2024-11-12 13:18:55 +0530

    couponId

--- src/routes/couponRoutes.js
+++ src/routes/couponRoutes.js
@@ -4,9 +4,9 @@ const couponController = require("../controllers/couponController");
 
 router.post("/", couponController.createCoupon);
 router.get("/", couponController.getAllCoupons);
-router.get("/:couponId", couponController.getCouponById);
-router.put("/:couponId", couponController.updateCoupon);
-router.delete("/:couponId", couponController.deleteCoupon);
+router.get("/:id", couponController.getCouponById);
+router.put("/:id", couponController.updateCoupon);
+router.delete("/:id", couponController.deleteCoupon);
 router.post("/applyCoupon", couponController.applyCouponToCart);
 
 module.exports = router;
