8ca0ad450efeb6cbb09eeac41a596a0a278b0096
Flutterneu <rsmx141@gmail.com>
2024-11-14 15:50:43 +0530

    new order

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-
-const MONGODB_URI = "mongodb+srv://vercel-admin-user:XM542odoAQkOSCxt@cluster0.f3uf4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI =
+  "mongodb+srv://vercel-admin-user:XM542odoAQkOSCxt@cluster0.f3uf4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      socketTimeoutMS: 45000,
+      serverSelectionTimeoutMS: 30000,
       // useCreateIndex: true, // Uncomment if using older versions of Mongoose
       // useFindAndModify: false, // Uncomment if using older versions of Mongoose
     });
