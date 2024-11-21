6bcd37a92527dfc9a919cbcbb1c9c5a8bd67c603
Flutterneu <rsmx141@gmail.com>
2024-11-21 16:30:31 +0530

    NODEMAILER uPDATED

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,6 +5,7 @@ const { admin } = require("../service/firebaseServices");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const AUTH_API_URL = "https://indigorhapsodyserver-h9a3.vercel.app/auth/login";
+const nodemailer = require("nodemailer");
 
 // Create a new user
 exports.createUser = async (req, res) => {
